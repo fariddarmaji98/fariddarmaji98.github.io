f3ad32e92dc061904d2294eebd8e694b21fe0381
darmaji.code.v <darmaji.code.v@gmail.com>
2022-06-02 22:50:53 +0700

    (test) code.v

---
+++ precache-manifest.a6ab0d264b29ae295b03a41e6c36f52e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0177affb240d27acdf3db520e4bf7949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e67260ae71d56c7d4b38",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b5b13fdd27347febe6d",
+    "url": "/static/js/2.f45617ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f45617ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67260ae71d56c7d4b38",
+    "url": "/static/js/main.61d4e017.chunk.js"
+  },
+  {
+    "revision": "54ff6f61acf9b1d227fb",
+    "url": "/static/js/runtime-main.35769ded.js"
+  }
+]);
